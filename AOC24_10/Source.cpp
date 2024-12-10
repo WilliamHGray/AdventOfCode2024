bce6a39a1e4a26bdd812479a5a65d243053c72bd
WilliamHGray <119584844+WilliamHGray@users.noreply.github.com>
2024-12-10 07:34:15 +0000

    Day 10

---
+++ AOC24_10/Source.cpp
@@ -0,0 +1,100 @@
+#include "Elves.h"
+#include <set>
+using namespace std;
+
+vector<vector<int>> moves = { {0,-1},{1,0},{0,1},{-1,0} };
+
+vector<vector<int>> makeMove(vector<vector<int>> positions, vector<string> grid, int nextNum)
+{
+	vector<vector<int>> nextPositions;
+	for (auto pos : positions)
+	{
+		for (auto move : moves)
+		{
+			vector<int> nextPos = pos + move;
+			if (checkPos(nextPos, grid))
+			{
+				int x = nextPos[0];
+				int y = nextPos[1];
+				if (grid[y][x] - '0' == nextNum)
+				{
+					nextPositions.push_back(nextPos);
+				}
+			}
+		}
+	}
+	return nextPositions;
+}
+
+vector<vector<vector<int>>> makeMove(vector<vector<vector<int>>> positions, vector<string> grid, int nextNum)
+{
+	vector<vector<vector<int>>> nextPositions;
+	for (auto pos : positions)
+	{
+		for (auto move : moves)
+		{
+			vector<int> nextPos = pos.back() + move;
+			if (checkPos(nextPos, grid))
+			{
+				int x = nextPos[0];
+				int y = nextPos[1];
+				if (grid[y][x] - '0' == nextNum)
+				{
+					vector<vector<int>> newPath = pos;
+					newPath.push_back(nextPos);
+					nextPositions.push_back(newPath);
+				}
+			}
+		}
+	}
+	return nextPositions;
+}
+
+int main() {
+	ifstream FileName;
+	FileName.open("aoc.txt");
+	string lineN;
+	vector<string> grid;
+	int total = 0;
+	int total1 = 0;
+	int total2 = 0;
+	vector<vector<int>> startPos;
+
+	while (getline(FileName, lineN))
+	{
+		grid.push_back(lineN);
+	}
+
+	for (int y = 0; y < grid.size(); y++)
+	{
+		for (int x = 0; x < grid[y].size(); x++)
+		{
+			if (grid[y][x] == '0')
+			{
+				startPos.push_back({ x,y });
+			}
+		}
+	}
+	
+	for (auto pos : startPos)
+	{
+		vector<vector<int>> nextMoves;
+		vector<vector<vector<int>>> paths;
+		nextMoves.push_back(pos);
+		paths.push_back({ pos });
+		for (int i = 1; i < 10; i++)
+		{
+			nextMoves = makeMove(nextMoves, grid, i);
+			paths = makeMove(paths, grid, i);
+		}
+		set<vector<int>>uniqueEnds;
+		for (auto ends : nextMoves)
+		{
+			uniqueEnds.insert(ends);
+		}
+		total += uniqueEnds.size();
+		total2 += paths.size();
+	}
+	cout << total << endl;
+	cout << total2 << endl;
+}
